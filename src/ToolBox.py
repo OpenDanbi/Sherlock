5b759b148ca37d67ced3af9772a661aa3c57039a
Bak Hyeonjae <bakhyeonjae@gmail.com>
2016-06-16 20:32:52 +0900

    Toolbox width fix

--- src/ToolBox.py
+++ src/ToolBox.py
@@ -12,12 +12,14 @@ class ToolBox(QVBoxLayout):
 
         self.sig.connect(self.addThreadList)
 
+        self.sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Expanding)
+
         self.groupBoxSearch = QGroupBox("Search messages")
         self.groupBoxSearch.setStyleSheet("QGroupBox {border: 1px solid gray; border-radius: 4px; };")
         vboxSearch = QVBoxLayout()
         self.searchHLayout = QHBoxLayout()
         self.editTextSearch = QTextEdit('')
-        self.editTextSearch.setFixedSize(100,30)
+        self.editTextSearch.setFixedSize(200,30)
         self.buttonSearch = QPushButton('Search')
         self.buttonSearch.setFixedSize(100,30)
         self.buttonSearch.clicked.connect(self.searchMsg)
@@ -28,19 +30,24 @@ class ToolBox(QVBoxLayout):
         vboxSearch.addLayout(self.searchHLayout)
         self.browseHLayout = QHBoxLayout()
         self.buttonLookUp = QPushButton('\u21e7')  #Arrow up
+        self.buttonLookUp.setFixedWidth(100)
         self.buttonLookUp.clicked.connect(self.moveToPrev)
         self.buttonLookDown = QPushButton('\u21e9') #Arrow down
+        self.buttonLookDown.setFixedWidth(100)
         self.buttonLookDown.clicked.connect(self.moveToNext)
         self.browseHLayout.addWidget(self.buttonLookUp)
         self.browseHLayout.addWidget(self.buttonLookDown)
         vboxSearch.addLayout(self.browseHLayout)
         self.groupBoxSearch.setLayout(vboxSearch)
         self.addWidget(self.groupBoxSearch)
+        self.groupBoxSearch.setSizePolicy(QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed))
 
         self.buttonShowAll = QPushButton('Show All')
+        self.buttonShowAll.setFixedWidth(200)
         self.buttonShowAll.clicked.connect(self.notifyShowAll)
         self.addWidget(self.buttonShowAll)
         self.buttonCapture = QPushButton('Capture')
+        self.buttonCapture.setFixedWidth(200)
         self.buttonCapture.clicked.connect(self.notifyCapture)
         self.addWidget(self.buttonCapture)
         self.msgRcv = []
@@ -50,12 +57,15 @@ class ToolBox(QVBoxLayout):
         vbox = QVBoxLayout()
         vbox.addWidget(self.msgInfo)
         self.buttonSrcView = QPushButton('view code')
+        self.buttonSrcView.setFixedWidth(200)
         self.buttonSrcView.clicked.connect(self.openSourceViewer)
         vbox.addWidget(self.buttonSrcView)
         self.buttonHide = QPushButton('Hide')
+        self.buttonHide.setFixedWidth(200)
         self.buttonHide.clicked.connect(self.notifyHide)
         vbox.addWidget(self.buttonHide)
         self.buttonHideAllMsg = QPushButton('Hide All')
+        self.buttonHideAllMsg.setFixedWidth(200)
         self.buttonHideAllMsg.clicked.connect(self.hideAllMsgNamedAsSelected)
         vbox.addWidget(self.buttonHideAllMsg)
         self.groupBoxMessageInfo.setLayout(vbox)
@@ -63,8 +73,8 @@ class ToolBox(QVBoxLayout):
         self.checkHideCircular.setCheckState(QtCore.Qt.Unchecked)
         self.checkHideCircular.stateChanged.connect(self.changeHideCircularMessage)
         self.addWidget(self.checkHideCircular)
-
         self.addWidget(self.groupBoxMessageInfo)
+        self.groupBoxMessageInfo.setSizePolicy(self.sizePolicy)
 
     def setMsgInfoMessage(self,msg):
         self.strMessage = msg
@@ -117,6 +127,7 @@ class ToolBox(QVBoxLayout):
         vbox.addWidget(self.listThread)
         self.groupBoxThreadInfo.setLayout(vbox)
         self.addWidget(self.groupBoxThreadInfo)
+        self.groupBoxThreadInfo.setSizePolicy(self.sizePolicy)
 
         for id in threads:
             item = QtGui.QListWidgetItem(id)
